e91077e63cfd6b0fa3b8cc2ecdcce383a7221d1a
praveen35 <praveenroyal35@gmail.com>
2016-04-26 22:39:11 -0700

    Added files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+list=["praveen",10,20,"pranith","sujani",20,30,40]
+tinylist=['prasana',20]
+
+print(list)
+print(tinylist)
+print(list[0])
+print(list[2:5])
+print(tinylist * 2)
+print(list[2:])
